85fbffa34bcf3637613e68df981988f4a0629664
Kashif <kashifdeshmukh786@gmail.com>
2026-02-21 00:52:41 +0530

    Enable SoundToggle component in the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         <SignatureExperience />
         <Gallery />
       </main>
-      {/* <SoundToggle /> */}
+      <SoundToggle />
     </LenisProvider>
   );
 }
